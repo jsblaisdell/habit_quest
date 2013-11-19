7d15460f644e4df273d8f1b6d4fbd3238b9f99cf
Justin Blaisdell <jsblaisdell@gmail.com>
2013-11-18 22:44:54 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HabitQuest::Application.config.secret_key_base = 'c43f18591c79fec6adf7edad44b4aca28c66c5702655bdc1a76e373ee2f70de6fe465bd1f7ae717287d92fcf1c776dad994c3e10bfc0ea808c6d2054f51e4772'
